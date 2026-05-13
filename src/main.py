fc2194036acea3e202f1f5e904f635bab5ea671e
busyCookie <s.livvan.k+busyCookie@gmail.com>
2026-05-13 16:24:44 +0300

    main updated to generatate html form avaialbel content and template;

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from textnode import TextType, TextNode
 from htmlnode import HTMLNode
-from utilities import text_to_textnodes, refesh_content
+from utilities import refesh_content, generate_page
 
 def main():
     refesh_content("./static/", "./public/")
+    generate_page("./content/index.md", "./template.html", "./public/index.html")
 
 if __name__ == "__main__":
     main()
